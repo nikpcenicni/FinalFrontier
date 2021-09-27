76ce9295fa45729a1409ee07cf9fabaed90287b7
nikpcenicni <nik.pcenicni@icloud.com>
2021-09-27 14:34:16 -0400

    Jumping mechanic

---
+++ Assets/platformer.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformer : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpforce;
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float rememberGroundedFor;
+    float lastTimeGrounded;
+    public int defaultAdditionalJumps = 1;
+    int additionalJumps;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Move();
+         Jump();
+         BetterJump();
+         CheckIfGrounded();
+    }
+    
+    void Move(){
+    	float x = Input.GetAxisRaw("Horizontal");
+    	float moveBy = x * speed;
+    	rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+    
+    void Jump(){
+    	if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor || additionalJumps > 0)){
+    	    rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+    	    additionalJumps--;
+    	}
+    }
+    
+    void CheckIfGrounded() {
+	Collider2D collider = Physics2D.OverlapCircle( isGroundedChecker.position, checkGroundRadius, groundLayer );
+	if (collider != null) {
+	     isGrounded = true;
+	     additionalJumps = defaultAdditionalJumps;
+	} else {
+	     if (isGrounded) {
+	     	lastTimeGrounded = Time.time;
+	     }
+	     isGrounded = false;
+	}
+    }
+    
+    void BetterJump() {
+    	if (rb.velocity.y < 0) {
+    	     rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+    	} else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) {
+    	     rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+    	}  
+    }
+}
