620470ccafcdf5c14e9c76e50fbb332cae5261cb
nikpcenicni <nik.pcenicni@icloud.com>
2021-09-28 16:35:39 -0400

    Added player camera tracking
    Added camera tracking to the player, in the main scene.
    
    There is bug when you reach the end of the ground on the right side where the visuals start to bug out..

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    
+    public Transform target;
+    public float smoothTime = 0.3f;
+    public float posY;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    private Vector3 velocity = Vector3.zero;
+    void Update() {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, posY, -10));
+        Vector3 desiredPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        minXUpdate(targetPosition);
+        transform.position = new Vector3(Mathf.Clamp(desiredPosition.x, minX, maxX), Mathf.Clamp(desiredPosition.y, minY, maxY), desiredPosition.z); 
+    }
+
+    void minXUpdate(Vector3 targetPosition) {
+        if (targetPosition.x > minX)
+            minX = targetPosition.x;
+    }
+}
