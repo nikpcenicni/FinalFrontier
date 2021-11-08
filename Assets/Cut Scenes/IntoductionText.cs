f8b51b6c27990ce3d6ad02b0f61682c78bb5903d
nikpcenicni <82239765+nikpcenicni@users.noreply.github.com>
2021-11-08 14:40:27 -0500

    Added new scene for begining of each save
    Can copy and paste the canvas to other scene in order to add cut scenes and dialog with other players that change on click. add phrases to the list by clicking on the canvas and looking for the introductiontext script on the right side and add phrases to the array inside the editor.

---
+++ Assets/Cut Scenes/IntoductionText.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class IntoductionText : MonoBehaviour
+{
+    public TextMeshProUGUI textBox;
+    public string[] phrases = new string[1];
+    public int phraseCounter = 0;
+
+    void Start(){
+        textBox.SetText(phrases[0]);
+    }
+
+    public void nextPhrase(){
+        phraseCounter++;
+        if (phraseCounter < phrases.Length)
+            textBox.SetText(phrases[phraseCounter]);  
+    }
+}
