e59ce145a0e685f809110dbb931cdf418171a949
GregHempel <ghempel@laurentian.ca>
2021-11-19 11:37:21 -0500

    Save Progress on menu
    Got the coins and health to show for each save slot on the menu.

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour{
     public static int saveNumber;
+    public TextMeshProUGUI health1;
+    public TextMeshProUGUI coins1;
+    public TextMeshProUGUI health2;
+    public TextMeshProUGUI coins2;
+    public TextMeshProUGUI health3;
+    public TextMeshProUGUI coins3;
+    
     
     public void playGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,7 +22,21 @@ public class MainMenu : MonoBehaviour{
     public void quitGame() {
         Application.Quit();
     }
+    public void load() {
+        MainMenu.saveNumber = 0;
+        PlayerData data = SaveSystem.loadPlayer();
+        health1.text = data.health.ToString();
+        coins1.text = data.bank.ToString();
+        MainMenu.saveNumber = 1;
+        data = SaveSystem.loadPlayer();
+        health2.text = data.health.ToString();
+        coins2.text = data.bank.ToString();
+        MainMenu.saveNumber = 2;
+        data = SaveSystem.loadPlayer();
+        health3.text = data.health.ToString();
+        coins3.text = data.bank.ToString();
 
+    }
     public void firstSave() {
         MainMenu.saveNumber = 0;
     }
