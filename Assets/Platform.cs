056a7e263954a75b9420b14f08a41c1aa01bbd94
GregHempel <ghempel@laurentian.ca>
2021-11-20 01:26:45 -0500

    Fixed the spaceship sprite
    There was 2 pixels on the spaceship that weren't present and looked weird and I fixed it, I thought it was a glitch but I messed up making the sprite lol

---
+++ Assets/Platform.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    void OnCollisionEnter2D (Collision2D col){
+        if(col.gameObject.name.Equals ("Player")){
+            Invoke("DropPlatform", 0.5f);
+            Destroy (gameObject, 2f);
+        }
+    }
+
+    void DropPlatform(){
+        rb.isKinematic = false;
+    }
+}
