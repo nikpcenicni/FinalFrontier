fcb4f213fa8c1ce81e9ca0515dd0c4ffef449b91
jeffreyfranklin <jfranklin1@laurentian.ca>
2021-11-26 22:13:31 -0500

    Fixing Error

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Platform : MonoBehaviour
 {
@@ -13,13 +14,25 @@ public class Platform : MonoBehaviour
     }
 
     void OnCollisionEnter2D (Collision2D col){
-        if(col.gameObject.name.Equals ("Player")){
-            Invoke("DropPlatform", 0.5f);
-            Destroy (gameObject, 2f);
+        if(col.gameObject.name.Equals("Player")){
+            StartCoroutine(DropPlatform());
+            Destroy(gameObject, 7f);
         }
     }
 
-    void DropPlatform(){
-        rb.isKinematic = false;
+    IEnumerator DropPlatform(){
+        yield return new WaitForSeconds(1);
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        float t = 0f;
+        float speed = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / 1f;
+            speed = t * 3.71f;
+            rb.velocity = new Vector2(0f, -speed);
+            yield return null;
+        }
     }
 }
