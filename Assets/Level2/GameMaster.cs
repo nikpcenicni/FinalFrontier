6c28c3da9cc0ff2c728e0fda96a371e5b09b5738
arshjit pelia <arsh.pelia1@gmail.com>
2021-12-02 13:32:23 -0500

    Working on implementing a Respawn Point
    Done with the game master object but it currently doesnt work

---
+++ Assets/Level2/GameMaster.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster gm;
+
+    private void Start()
+    {
+        if(gm == null)
+        {
+            gm = this;
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2; 
+
+    public IEnumerator RespawnPlayer()
+    {
+        Debug.Log("TODO: ADD SPAWN PARTICLES");
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("TODO: ADD SPAWN PARTICLES");
+    }
+
+    public static void KillPlayer(Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine (gm.RespawnPlayer());
+    }
+}
