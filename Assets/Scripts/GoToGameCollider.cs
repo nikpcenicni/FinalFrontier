a731a4beb122c098fc4fd1902bd22982c1a987b7
Nikholas Pcenicni <npcenicni@laurentian.ca>
2021-12-05 21:12:37 -0500

    Scene transfer on space ship and in game both work

--- Assets/Scripts/GoToGameCollider.cs
+++ Assets/Scripts/GoToGameCollider.cs
@@ -6,11 +6,17 @@ using UnityEngine.SceneManagement;
 public class GoToGameCollider : MonoBehaviour
 {
     public Player player;
+
+    void Update(){
+        player = FindObjectOfType<Player>();
+    }
     // Start is called before the first frame update
     void OnTriggerEnter2D() {
-        if (player.level == "Red Planet")
+        if (player.level == "Red Planet" && SceneManager.GetActiveScene().name == "SpaceShip")
             SceneManager.LoadScene("RedPlanet");
-        if (player.level == "Forest Planet")
+        if (player.level == "Forest Planet" && SceneManager.GetActiveScene().name == "SpaceShip")
             SceneManager.LoadScene("ForestPlanet");
+        else
+             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 }
